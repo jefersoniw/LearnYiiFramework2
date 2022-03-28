7a8fe9841f3cbfd13a32b3452b5dc06b50fcfb88
jefersoniw <jeferson_chagas25@hotmail.com>
2022-03-28 14:21:37 -0300

    A Aplicação, explicação dos itens de propriedades e metodos para serem usados no web.php

--- config/params.php
+++ config/params.php
@@ -4,4 +4,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'endereco' => 'rua x, exemplo logradouro, cep xxxxx-xx'
 ];
--- config/web.php
+++ config/web.php
@@ -5,12 +5,17 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'name' => 'Minha Aplicação',
+    'version' => '2.5',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'language' => 'pt-BR',
+    'sourceLanguage' => 'pt-BR',
+    'timeZone' => 'America/Bahia',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
